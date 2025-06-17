79c0e776f368d165cc8fe480d463ff3970e27be1
shahadot99999 <shahadot99999@gmail.com>
2025-06-17 20:14:55 +0600

    Use google sign in

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,21 @@
-import { GoogleAuthProvider } from "firebase/auth";
+// import { GoogleAuthProvider } from "firebase/auth";
+// import { signInWithPopup } from "firebase/auth/web-extension";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from "../../firebase/firebase.init"
 
 const Login = () => {
   
     const provider = new GoogleAuthProvider();
 
     const handleGoogleSignIn =()=>{
-        console.log('google sign in');
+        //console.log('google sign in');
+        signInWithPopup(auth, provider)
+        .then((result)=>{
+            console.log(result);
+        })
+        .catch(error =>{
+            console.log(error);
+        })
     }
     return (
         <div>
